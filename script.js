9a0a771bfd80fbe1e7b627c19ee550740db0df80
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-11 12:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species;
+		this._species = species;
 	}
 
 	get species(){
-		return this.species;
+		return this._species;
 	}
 	makeSound(){
-		console.log(`The ${this.species} makes a sound`)
+		console.log(`The ${this._species} makes a sound`)
 	}
 }
 
